1755f91e83f40dbc9ea422bd45111bd2814f60c3
DannyMyles <dancun.wawire@student.moringaschool.com>
2021-12-02 18:20:05 +0300

    added  models

--- posts/models.py
+++ posts/models.py
@@ -21,26 +21,3 @@ class Post(models.Model):
     def __str__(self):
         """Return title and username"""
         return "{} by @{}".format(self.title, self.profile.user.username)
-
-
-# class User(models.Model):
-#     """User model."""
-
-#     email = models.EmailField(unique=True)
-#     password = models.CharField(max_length=100)
-
-#     first_name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
-
-#     is_admin = models.BooleanField(default=False)
-
-#     bio = models.TextField(blank=True)
-
-#     birthdate = models.DateField(blank=True, null=True)
-
-#     created = models.DateTimeField(auto_now_add=True)
-#     modified = models.DateTimeField(auto_now=True)
-
-#     def __str__(self):
-#         """Return email."""
-#         return self.email
