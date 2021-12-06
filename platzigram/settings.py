7f6a416fa0cf45dc1565485141490b04031f967b
DannyMyles <dancun.wawire@student.moringaschool.com>
2021-12-06 13:57:47 +0300

    added static

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -12,15 +12,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kloudless.settings")
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
-Share
-Improve this answer
-Follow
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
